aae3f38898e91e5b865564cf1ceb9a03cb25432f
Britton Baumann <brittonbaumann@gmail.com>
2016-04-22 22:25:01 -0500

    Assignment Rails Signing Up: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :topics do
-    resources :posts, except: [:index]
+  resources :posts, except: [:index]
   end
 
    resources :users, only: [:new, :create]
+
+post 'user/confirm' => 'users#confirm'
 
 get 'about' => 'welcome#about'
 
