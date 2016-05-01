ce5d836547858ed1164935a34d1ddf30fb1b6c1c
Britton Baumann <brittonbaumann@gmail.com>
2016-04-30 22:16:30 -0500

    Assignment Rails Favorites: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,7 +2,7 @@ class FavoriteMailer < ApplicationMailer
   default from: "brittonbaumann@gmail.com"
 
   def new_comment(user, post, comment)
- 
+
      headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
      headers["References"] = "<post/#{post.id}@your-app-name.example>"
@@ -12,5 +12,15 @@ class FavoriteMailer < ApplicationMailer
      @comment = comment
 
      mail(to: user.email, subject: "New comment on #{post.title}")
+   end
+
+   def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+     @post = post
+
+     mail(to: post.user.email, subject: "New favorite on #{post.title}")
    end
 end
