1b308fa595f6401b6e7f8b6d5258602bd88ccad9
Britton Baumann <brittonbaumann@gmail.com>
2016-05-01 15:00:41 -0500

    Assignment Rails Public Profiles: completed

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,7 @@
 module FavoritesHelper
+
+  def user_favorites?(user)
+    user.favorites.count > 0
+  end
+
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
         current_user && (current_user == post.user || current_user.admin?)
    end
+
+   def user_posts?(user)
+     user.posts.count > 0
+   end
 end
