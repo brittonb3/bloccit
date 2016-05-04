d5136af84fdb9a08dbb848e99674e186f6004f0d
Britton Baumann <brittonbaumann@gmail.com>
2016-05-03 22:06:59 -0500

    Checkpoint Rails API 1: completed

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,11 @@ get 'about' => 'welcome#about'
 
   root 'welcome#index'
 
+  namespace :api do
+     namespace :v1 do
+       resources :users, only: [:index, :show]
+       resources :topics, only: [:index, :show]
+     end
+   end
+
 end
