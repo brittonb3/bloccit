68a31ac1e86d830d53124e8d2860f4bc4f453c02
Britton Baumann <brittonbaumann@gmail.com>
2017-01-06 13:08:04 -0600

    took out you have to be admin to create topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -71,10 +71,10 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
-       redirect_to topics_path
-     end
+    #  unless current_user.admin?
+    #    flash[:alert] = "You must be an admin to do that."
+    #    redirect_to topics_path
+    #  end
    end
 
 end
